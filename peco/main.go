ef51a3873aae97e8dd61282e6e0ebbc61177efd7
soeyusuke <s1240231@gmail.com>
2018-01-21 19:57:40 +0900

    [ADD] format message.text

---
+++ peco/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+type Peco struct {
+	bot      *linebot.Client
+	favoRate int
+}
+
+func main() {
+	peco, err := NewClient()
+	if err != nil {
+		log.Print(err)
+	}
+
+	http.HandleFunc("/callback", peco.Callback)
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewClient() (*Peco, error) {
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Peco{
+		bot: bot,
+	}, nil
+}
+
+func (peco *Peco) Callback(w http.ResponseWriter, req *http.Request) {
+	events, err := peco.bot.ParseRequest(req)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if err := peco.textResponse(message, event.ReplyToken); err != nil {
+					log.Print(err)
+				}
+			case *linebot.LocationMessage:
+			default:
+			}
+
+		case linebot.EventTypePostback:
+			switch event.Postback.Data {
+			case "":
+			default:
+			}
+		}
+	}
+}
+
+func (peco *Peco) textResponse(message *linebot.TextMessage, reply string) error {
+	var err error
+	switch message.Text {
+	case "ご飯行かない？":
+		if _, err = peco.bot.ReplyMessage(
+			reply,
+			linebot.NewTextMessage("行きたい！行きたい！"),
+			ButtonTemplate2(button2[message.Text]),
+		).Do(); err != nil {
+			log.Print(err)
+		}
+	case "いただきます！":
+		if _, err = peco.bot.ReplyMessage(
+			reply,
+			ButtonTemplate(button[message.Text]),
+		).Do(); err != nil {
+			log.Print(err)
+		}
+	case "ごちそうさま！":
+		if _, err = peco.bot.ReplyMessage(
+			reply,
+			linebot.NewTextMessage("ごちそうさまでした"),
+			ButtonTemplate4(button4[message.Text]),
+		).Do(); err != nil {
+			log.Print(err)
+		}
+	case "お話しよう！":
+		if _, err = peco.bot.ReplyMessage(
+			reply,
+			ButtonTemplate4(talk[rand.Intn(3)]),
+		).Do(); err != nil {
+			log.Print(err)
+		}
+	case "（好感度）":
+		if _, err = peco.bot.ReplyMessage(
+			reply,
+			linebot.NewTextMessage(formatStr("好感度: ", peco.favoRate)),
+		).Do(); err != nil {
+			log.Print(err)
+		}
+	default:
+	}
+	return nil
+}
+
+func ButtonTemplate(res buttonTemp) *linebot.TemplateMessage {
+	temp := linebot.NewButtonsTemplate(
+		res.image,
+		res.title,
+		res.label,
+		linebot.NewPostbackTemplateAction(" ", " ", ""),
+	)
+	return linebot.NewTemplateMessage("button", temp)
+}
+
+func ButtonTemplate2(res buttonTemp2) *linebot.TemplateMessage {
+	temp := linebot.NewButtonsTemplate(
+		res.image,
+		res.title,
+		res.label,
+		linebot.NewPostbackTemplateAction(res.select1, formatStr(res.key, 1), ""),
+		linebot.NewPostbackTemplateAction(res.select2, formatStr(res.key, 2), ""),
+	)
+	return linebot.NewTemplateMessage("button2", temp)
+}
+
+func ButtonTemplate4(res buttonTemp4) *linebot.TemplateMessage {
+	temp := linebot.NewButtonsTemplate(
+		res.image,
+		res.title,
+		res.label,
+		linebot.NewPostbackTemplateAction(res.select1, formatStr(res.key, 1), ""),
+		linebot.NewPostbackTemplateAction(res.select2, formatStr(res.key, 2), ""),
+		linebot.NewPostbackTemplateAction(res.select3, formatStr(res.key, 3), ""),
+		linebot.NewPostbackTemplateAction(res.select4, formatStr(res.key, 4), ""),
+	)
+	return linebot.NewTemplateMessage("confilm", temp)
+}
+
+func formatStr(str string, i int) string {
+	return fmt.Sprintf("%s%d", str, i)
+}
---
+++ peco/types.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type buttonTemp struct {
+	image string
+	title string
+	label string
+}
+
+type buttonTemp2 struct {
+	key     string
+	image   string
+	title   string
+	label   string
+	select1 string
+	select2 string
+}
+
+type buttonTemp4 struct {
+	key     string
+	title   string
+	image   string
+	label   string
+	select1 string
+	select2 string
+	select3 string
+	select4 string
+}
+
+var (
+	button  map[string]buttonTemp
+	button2 map[string]buttonTemp2
+	button4 map[string]buttonTemp4
+	talk    map[int]buttonTemp4
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
+	button = map[string]buttonTemp{
+		"いただきます！": buttonTemp{
+			image: "https://i.imgur.com/97XRjTa.png",
+			title: "いただきます♪",
+			label: " ",
+		},
+	}
+
+	button2 = map[string]buttonTemp2{
+		"ご飯行かない？": buttonTemp2{
+			key:     "meshi",
+			image:   "https://i.imgur.com/tNxL35o.png",
+			title:   "しおえってお店行ってみたいなぁ♪",
+			label:   " ",
+			select1: "行く",
+			select2: "行かない",
+		},
+		"location": buttonTemp2{
+			key:     "osusume",
+			image:   "https://i.imgur.com/AZ9L8d6.png",
+			title:   "待ってたよ〜〜、お腹すいちゃった😂",
+			label:   "オススメを聞く？聞かない？",
+			select1: "聞く",
+			select2: "聞かない",
+		},
+	}
+
+	button4 = map[string]buttonTemp4{
+		"": buttonTemp4{
+			key:     "",
+			title:   "",
+			image:   "",
+			label:   "",
+			select1: "",
+			select2: "",
+			select3: "",
+			select4: "",
+		},
+		"": buttonTemp4{
+			key:     "",
+			title:   "",
+			image:   "",
+			label:   "",
+			select1: "",
+			select2: "",
+			select3: "",
+			select4: "",
+		},
+	}
+
+	talk = map[int]buttonTemp4{
+		0: buttonTemp4{
+			key:     "shiro",
+			title:   "なんのお話にする？",
+			image:   "https://i.imgur.com/iazlG5a.png",
+			label:   " ",
+			select1: "大阪城？",
+			select2: "鶴ヶ城？",
+			select3: "名古屋城？",
+			select4: "カリオストロの城？",
+		},
+		1: buttonTemp4{
+			key:     "kyodo",
+			title:   "なんの話にする？",
+			image:   "https://i.imgur.com/iazlG5a.png",
+			label:   " ",
+			select1: "ちゃんちゃんやき", //北海道の郷土料理だけどあんまわかんないや
+			select2: "かにまき汁",    // 宮崎県の郷土料理だけどあんまわかんないや
+			select3: "イノシシカレー",  // 山梨県の郷土料理だけどあんまわかんないや
+			select4: "こづゆ",
+		},
+		2: buttonTemp4{
+			key:     "men",
+			title:   "なんの話にする？",
+			image:   "https://i.imgur.com/iazlG5a.png",
+			label:   " ",
+			select1: "喜多方ラーメン",
+			select2: "白河ラーメン",
+			select3: "博多ラーメン",
+			select4: "札幌ラーメン",
+		},
+	}
+
+}
